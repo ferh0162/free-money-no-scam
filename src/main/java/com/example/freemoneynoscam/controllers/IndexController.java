81a2d44fc2ca051a1551d1997195038445db6ddb
ferh0162 <ferh0162@stud.kea.dk>
2022-09-14 13:57:20 +0200

    Email bliver gemt og vist på næste side. Men har ikke kunne gemme dem i noget repository

--- src/main/java/com/example/freemoneynoscam/controllers/IndexController.java
+++ src/main/java/com/example/freemoneynoscam/controllers/IndexController.java
@@ -1,11 +1,13 @@
 package com.example.freemoneynoscam.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class IndexController {
@@ -15,12 +17,15 @@ public class IndexController {
     }
 
     @PostMapping("/test")
-    public String test(WebRequest dataFromForm){
-        System.out.println(dataFromForm.getParameter("email"));
-        System.out.println(dataFromForm.getParameter("email"));
-        System.out.println(dataFromForm.getParameter("email"));
-        System.out.println(dataFromForm.getParameter("email"));
-        System.out.println(dataFromForm.getParameter("email"));
-        return "redirect:/";
+    public String test(/*WebRequest dataFromForm*/@RequestParam("email") String email, RedirectAttributes redirectAttributes){
+        //System.out.println(dataFromForm.getParameter("email"));
+        redirectAttributes.addAttribute("email",email);
+
+        return "redirect:/test";
+    }
+    @GetMapping("/test")
+    public String lavSuccessite(Model model, @RequestParam("email") String email){
+        model.addAttribute("email",email);
+        return "successite";
     }
 }
